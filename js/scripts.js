468ebb33179ec37fe4a229b891d60f9e77b30c88
AlanMFlores <alanmflores.13@gmail.com>
2021-03-03 21:31:28 -0300

    Estilos responsive

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+const toggle = document.querySelector('header__menu--hamburger');
+const menuResponsive = document.querySelector('haeder__responsive');
+
+function() {
+    toggle.addEventListener(onclick)
+}
